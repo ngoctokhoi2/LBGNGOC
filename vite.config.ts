780d9ecd6d14a5d03a315c689e779296f4a0f1b9
ngoctokhoi2 <ngoctokhoi2@gmail.com>
2026-09-18 18:38:25 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_ACTIONS ? '/LBGNGOC/' : '/',
+    base: '/TKBNGOC/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
